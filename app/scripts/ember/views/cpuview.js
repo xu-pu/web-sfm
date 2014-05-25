b7541aa47456b38b1963234e214b0343feb637fa
__PTX__ <sheep9275@gmail.com>
2014-05-26 05:46:06 +0800

    cpu setting

---
+++ app/scripts/ember/views/cpuview.js
@@ -0,0 +1,51 @@
+'use strict';
+
+App.CpuSettingView = Ember.View.extend({
+
+    templateName: 'widgets/cpu-setting',
+
+    tagName: 'div',
+
+    classNames: [
+        'controll-panel__cpu-setting'
+    ],
+
+    hoverAt: 4,
+
+    selected: 4,
+
+    NodeView: Ember.View.extend({
+
+        templateName: 'widgets/cpu-node',
+
+        tagName: 'div',
+
+        classNames: [
+            'controll-panel__cpu-setting__node'
+        ],
+
+        index: null,
+
+        isEnabled: function(){
+            return this.get('index') < this.get('parentView').get('selected');
+        }.property('parentView.selected'),
+
+        willEnable: function(){
+            return (!this.get('isEnabled')) && (this.get('index') <= this.get('parentView').get('hoverAt'));
+        }.property('isEnabled', 'parentView.hoverAt'),
+
+        willDisable: function(){
+            return this.get('isEnabled') && (this.get('index') > this.get('parentView').get('hoverAt'));
+        }.property('isEnabled', 'parentView.hoverAt'),
+
+        mouseEnter: function(){
+            this.get('parentView').set('hoverAt', this.get('index'));
+        },
+
+        click: function(){
+            this.get('parentView').set('selected', this.get('index')+1);
+        }
+
+    })
+
+});
