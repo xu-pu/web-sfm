321b12df0697be9cd0e3e1919c4156ed9624af1a
__PTX__ <sheep9275@gmail.com>
2014-06-01 03:06:52 +0800

    style tweek

--- app/scripts/ember/views/twoviewgridview.js
+++ app/scripts/ember/views/twoviewgridview.js
@@ -8,15 +8,37 @@ App.TwoViewGridView = Ember.View.extend({
         'main-container__match-table'
     ],
 
+    hover: null,
+
     templateName: 'widgets/twoviewgrid',
 
     NodeView: Ember.View.extend({
 
         tagName: 'th',
 
+        mouseEnter: function(){
+            this.get('parentView').set('hover', this);
+        },
+
+        isHorizontal: function(){
+            return this.get('parentView.hover.indexY') === this.get('indexY') && this.get('parentView.hover.indexX') > this.get('indexX');
+        }.property('parentView.hover'),
+
+        isVertical: function(){
+            return this.get('parentView.hover.indexX') === this.get('indexX') && this.get('parentView.hover.indexY') > this.get('indexY');
+        }.property('parentView.hover'),
+
         viewX: null,
 
         viewY: null,
+
+        indexX: function(){
+            return this.get('controller.images').indexOf(this.get('viewX'));
+        }.property('viewX'),
+
+        indexY: function(){
+            return this.get('controller.images').indexOf(this.get('viewY'));
+        }.property('viewY'),
 
         isFinished: false,
 
