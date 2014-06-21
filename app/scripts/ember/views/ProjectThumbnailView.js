2437bdbb7bd6d97af8926c611f0b1be23f8c4fbe
sheep9275 <sheep9275@gmail.com>
2014-06-21 14:52:08 +0800

    minor

---
+++ app/scripts/ember/views/ProjectThumbnailView.js
@@ -0,0 +1,11 @@
+App.ProjectThumbnailView = Ember.View.extend({
+
+    tagName: 'li',
+
+    templateName: 'widgets/project-thumbnail',
+
+    classNames: [
+        'project-thumbnail'
+    ]
+
+});
