cf3eef235c80504cb8d0074155edfe6210d3adc0
ptx-pluto <sheep9275@gmail.com>
2014-09-18 00:25:06 +0800

    Rectification algorithm fixed after researched R and t

--- src/webmvs/rectification.js
+++ src/webmvs/rectification.js
@@ -1,3 +1,9 @@
+'use strict';
+
+/**
+ * @typedef {{R, t}} CalibratedCamera
+ */
+
 var pool = require('ndarray-scratch');
 var la = require('sylvester'),
     Matrix = la.Matrix,
@@ -5,13 +11,9 @@ var la = require('sylvester'),
 
 module.exports = rectification;
 
-function rectification(r1, r2, t1, t2, f1, f2){
-    r1 = Matrix.create(r1);
-    r2 = Matrix.create(r2);
-    t1 = Vector.create(t1);
-    t2 = Vector.create(t2);
-    var R = r1.inverse().multiply(r2);
-    var t = t2.subtract(t1);
+function rectification(R1, R2, t1, t2, f1, f2){
+    var R = R2.x(R1.transpose());
+    var t = t2.subtract(R.x(t1));
 
     var ratio = f1/t.elements[2];
     var T = Vector.create([ratio*t.elements[0], ratio*t.elements[1], 1]);
@@ -20,7 +22,7 @@ function rectification(r1, r2, t1, t2, f1, f2){
     var e2 = normalize(Vector.create([-t.elements[1], t.elements[0], 0]));
     var e3 = normalize(e1.cross(e2));
     var rect = Matrix.create([e1.elements, e2.elements, e3.elements]);
-    return [rect, rect.multiply(R)]; // R1, R2
+    return [rect, R.multiply(rect)]; // new R1, R2
 }
 
 function normalize(v){
