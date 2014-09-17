99a0bda0660592b26c668e68e4481e23d16d8d38
ptx-pluto <sheep9275@gmail.com>
2014-09-18 01:15:10 +0800

    Projection matrix added

---
+++ src/math/projections.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector;
+
+module.exports.getProjectionMatrix = getProjectionMatrix;
+
+function getProjectionMatrix(R, t, focal, width, height){
+
+    var K = Matrix.create([
+        [focal, 0    , width/2 , 0],
+        [0    , focal, height/2, 0],
+        [0    , 0    , 1       , 0]
+    ]);
+
+    var P = R.augment(t).transpose().augment(Vector.create([0,0,0,1])).transpose();
+
+    return K.x(P);
+
+}
