b3f4b4ba1d20ff0f5248a4aeeb138b11ec728729
ptx-pluto <sheep9275@gmail.com>
2014-09-21 23:59:47 +0800

    drawEpipolarGeometry added

---
+++ src/visualization/drawEpipolarGeometry.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector;
+
+var cord = require('../utils/cord.js');
+
+module.exports = function(config, ctx, matches, features1, features2, F){
+    var offsetX = config.offsetX,
+        offsetY = config.offsetY,
+        ratio1 = config.ratio1,
+        ratio2 = config.ratio2,
+        img1 = config.cam1,
+        img2 = config.cam2;
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    matches.map(function(match){
+        var f1 = features1[match[0]],
+            f2 = features2[match[1]],
+            p1 = Vector.create(cord.featureToImg(f1, img1)),
+            p2 = Vector.create(cord.featureToImg(f2, img2)),
+            line1 = cord.imgline2points(F.x(p2), img1.width, img1.height),
+            line2 = cord.imgline2points(F.transpose().x(p1), img2.width, img2.height);
+        if (line1.length === 2) {
+            ctx.moveTo(line1[0].col*ratio1, line1[0].row*ratio1);
+            ctx.lineTo(line1[1].col*ratio1, line1[1].row*ratio1);
+        }
+        if (line2.length === 2) {
+            ctx.moveTo(line2[0].col*ratio2+offsetX, line2[0].row*ratio2+offsetY);
+            ctx.lineTo(line2[1].col*ratio2+offsetX, line2[1].row*ratio2+offsetY);
+        }
+    });
+    ctx.stroke();
+};
--- tests/matching-test.js
+++ tests/matching-test.js
@@ -18,6 +18,7 @@ var samples = require('../src/utils/samples.js'),
     drawFeatures = require('../src/visualization/drawFeatures.js'),
     drawMatches = require('../src/visualization/drawMatches.js'),
     drawImagePair = require('../src/visualization/drawImagePair.js'),
+    drawEpipolarGeometry = require('../src/visualization/drawEpipolarGeometry.js'),
     cord = require('../src/utils/cord.js');
 
 
@@ -45,52 +46,13 @@ function promiseMatchingVisual(name, img1, img2, features1, features2, matches){
     return promiseWriteFile('/home/sheep/Code/'+name+'.png', canv.toBuffer());
 }
 
-function promiseEpipolarVisual(img1, img2, features1, features2, matches, F){
-    var canv1 = new Canvas(),
-        canv2 = new Canvas(),
-        ctx1 = canv1.getContext('2d'),
-        ctx2 = canv2.getContext('2d');
-    ctx1.strokeStyle = 'red';
-    ctx1.lineWidth = 10;
-    ctx2.strokeStyle = 'red';
-    ctx2.lineWidth = 10;
-    canv1.width = img1.width;
-    canv1.height = img1.height;
-    canv2.width = img2.width;
-    canv2.height = img2.height;
-    ctx1.drawImage(img1, 0, 0);
-    ctx2.drawImage(img2, 0, 0);
-    var epipolarLines = _.sample(matches, 30).map(function(match){
-        var f1 = features1[match[0]],
-            f2 = features2[match[1]],
-            p1 = Vector.create(cord.featureToImg(f1, img1)),
-            p2 = Vector.create(cord.featureToImg(f2, img2)),
-            line1 = F.x(p2),
-            line2 = F.transpose().x(p1);
-        return [
-            cord.imgline2points(line1, img1.width, img1.height),
-            cord.imgline2points(line2, img2.width, img2.height)
-        ];
-    });
-    epipolarLines.forEach(function(pair){
-        var line1 = pair[0],
-            line2 = pair[1];
-        if (line1.length === 2) {
-            ctx1.moveTo(line1[0].col, line1[0].row);
-            ctx1.lineTo(line1[1].col, line1[1].row);
-        }
-        if (line2.length === 2) {
-            ctx2.moveTo(line2[0].col, line2[0].row);
-            ctx2.lineTo(line2[1].col, line2[1].row);
-        }
-    });
-    ctx1.stroke();
-    ctx2.stroke();
+function promiseEpipolarVisual(path, img1, img2, features1, features2, matches, F){
+    var canv = new Canvas(),
+        config = drawImagePair(img1, img2, canv, 800),
+        ctx = canv.getContext('2d');
+    drawEpipolarGeometry(config, ctx, _.sample(matches, 30), features1, features2, F);
     console.log('epipolar lines rendered');
-    return Promise.all([
-        promiseWriteFile('/home/sheep/Code/epi1.png', canv1.toBuffer()),
-        promiseWriteFile('/home/sheep/Code/epi2.png', canv2.toBuffer())
-    ]);
+    return promiseWriteFile(path, canv.toBuffer());
 }
 
 
@@ -160,7 +122,7 @@ function promiseEpipolarGeometry(i1, i2){
         return Promise.all([
             promiseMatchingVisual('raw', results[0], results[1], features1, features2, matches),
             promiseMatchingVisual('filtered', results[0], results[1], features1, features2, result.dataset),
-            promiseEpipolarVisual(results[0], results[1], features1, features2, result.dataset, result.rel)
+            promiseEpipolarVisual('/home/sheep/Code/epipolar.png', results[0], results[1], features1, features2, result.dataset, result.rel)
         ]);
 
     }).then(function(){
@@ -168,20 +130,5 @@ function promiseEpipolarGeometry(i1, i2){
     });
 }
 
-promiseEpipolarGeometry(5,6);
+promiseEpipolarGeometry(3,4);
 
-//promisePair(1,2);
-
-//promisePair(3,4);
-
-//samples.promiseImage(1).then(function(img){
-//    samples.showGrayscale(img);
-//});
-
-//var matches = bruteforce(samples.getFeatures(1), samples.getFeatures(2), 0.8);
-//console.log(matches.length);
-
-//promiseWriteFile('/home/sheep/Code/test.json', JSON.stringify(matches)).then(function(){
-//    var obj = require('/home/sheep/Code/test.json');
-//    console.log(obj.length);
-//});
