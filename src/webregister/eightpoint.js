4e0a4d1f484890e8d737dafe093354f018bf7238
ptx-pluto <sheep9275@gmail.com>
2014-09-22 23:26:52 +0800

    error threshold test added

--- src/webregister/eightpoint.js
+++ src/webregister/eightpoint.js
@@ -68,11 +68,11 @@ function eightPoint(matches, metadata){
 
 
 /**
- * @param {SFM.Matrix} F
+ * @param F
  * @param {int[]} match
  * @param metadata
- * @param metadata.cam1
- * @param metadata.cam2
+ * @param {Camera} metadata.cam1
+ * @param {Camera} metadata.cam2
  * @param {Feature[]} metadata.features1
  * @param {Feature[]} metadata.features2
  * @return {number}
--- tests/matching-test.js
+++ tests/matching-test.js
@@ -105,20 +105,6 @@ function promiseEpipolarGeometry(i1, i2){
 
         console.log(result.dataset.length+'/'+matches.length+' passed RANSAC');
 
-/*
-        var filtered = ransac({
-            dataset: result.dataset,
-            metadata: metadata,
-            subset: 4,
-            relGenerator: homography,
-            errorGenerator: homography.homographyError,
-            outlierThreshold: 0.15,
-            errorThreshold: 500,
-            trials: 1000
-        });
-
-        console.log(result.dataset.length+'/'+result.dataset.length+' passed homography RANSCA');
-*/
         return Promise.all([
             promiseMatchingVisual('raw', results[0], results[1], features1, features2, matches),
             promiseMatchingVisual('filtered', results[0], results[1], features1, features2, result.dataset),
@@ -130,5 +116,61 @@ function promiseEpipolarGeometry(i1, i2){
     });
 }
 
-promiseEpipolarGeometry(3,4);
+function testErrorRange(i1, i2){
+    return Promise.all([
+        samples.promiseCanvasImage(i1),
+        samples.promiseCanvasImage(i2)
+    ]).then(function(results){
+
+        var features1 = samples.getFeatures(i1),
+            features2 = samples.getFeatures(i2),
+            matches = samples.getRawMatches(i1, i2),
+            metadata = {
+                cam1: results[0],
+                cam2: results[1],
+                features1: features1,
+                features2: features2
+            };
+
+        return Promise.all([0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08,0.09, 0.1].map(function(threshold){
+            try {
+                var result = ransac({
+                    dataset: matches,
+                    metadata: metadata,
+                    subset: 8,
+                    relGenerator: eightpoint,
+                    errorGenerator: eightpoint.fundamentalMatrixError,
+                    outlierThreshold: 0.2,
+                    errorThreshold: threshold,
+                    trials: 500
+                });
+
+                console.log(result.dataset.length+'/'+matches.length+' passed RANSAC');
+
+                return promiseEpipolarVisual('/home/sheep/Code/epipole/'+threshold+'.png',
+                    results[0], results[1], features1, features2, result.dataset, result.rel);
+
+            }
+            catch (err) {
+                console.log('Did not pass when threshold = ' + threshold);
+                return Promise.resolve();
+            }
+            /*
+            return Promise.all([
+                promiseMatchingVisual('raw', results[0], results[1], features1, features2, matches),
+                promiseMatchingVisual('filtered', results[0], results[1], features1, features2, result.dataset),
+             promiseEpipolarVisual('/home/sheep/Code/epipolar.png', results[0], results[1], features1, features2, result.dataset, result.rel)
+            ]);
+            */
+
+        }));
+
+    }).then(function(){
+        console.log('finished');
+    });
+}
+
+testErrorRange(2,3);
+
+//promiseEpipolarGeometry(3,4);
 
