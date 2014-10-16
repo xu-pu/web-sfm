c9e81d2d546c304c3d0cdaab0f1268be7a117862
ptx-pluto <sheep9275@gmail.com>
2014-10-16 21:35:34 +0800

    sift detector modified

--- src/websift/detector.js
+++ src/websift/detector.js
@@ -2,61 +2,57 @@
 
 var _ = require('underscore');
 
-var siftOrientation = require('./orientation.js');
-
-module.exports = siftDetector;
+module.exports = detect;
 
 /**
- * Find keypoints from the DOGs of one scale space
- * @param dogs
- * @param {Number} octave
+ * @param {DoG[]} dogs
  * @param {Function} callback
  */
-function siftDetector(dogs, octave, callback){
+function detect(dogs, callback){
 
     console.log('detecting feature points');
 
-    var width = Math.ceil(dogs[0].img.shape[1]/2),
-        height = Math.ceil(dogs[0].img.shape[0]/2),
+    var imgs = dogs.map(function(dog){ return dog.img; }),
+        layer = dogs[1],
+        width = layer.shape[1],
+        height = layer.shape[0],
         contrastWindow = [-1,0,1];
 
-    _.range(1, dogs.length-1).forEach(function(layer){
+    _.range(height).forEach(function(row){
+        _.range(width).forEach(function(col){
 
-        _.range(height).forEach(function(row){
-            _.range(width).forEach(function(col){
+            var center = layer.get(row,col);
+            var max = null;
+            var min = null;
 
-                var center = dogs[layer].img.get(row,col);
-                var max = null;
-                var min = null;
-
-                var isLimit = contrastWindow.every(function(x){
-                    return contrastWindow.every(function(y){
-                        return contrastWindow.every(function(z){
-                            if(x===0 && y===0 && z===0) {
-                                return true;
+            var isLimit = contrastWindow.every(function(x){
+                return contrastWindow.every(function(y){
+                    return contrastWindow.every(function(z){
+                        if(x===0 && y===0 && z===0) {
+                            return true;
+                        }
+                        else {
+                            var cursor = imgs[1+z].get(row+y, col+x);
+                            if (cursor > center && (max === null || cursor > max)) {
+                                max = cursor;
                             }
-                            else {
-                                var cursor = dogs[layer+z].img.get(row+y, col+x);
-                                if (cursor > center && (max === null || cursor > max)) {
-                                    max = cursor;
-                                }
-                                else if (cursor < center && (min === null || cursor < min)) {
-                                    min = cursor;
-                                }
-                                else if (cursor === center) {
-                                    return false;
-                                }
-                                return (max === null || min === null);
+                            else if (cursor < center && (min === null || cursor < min)) {
+                                min = cursor;
                             }
-                        });
+                            else if (cursor === center) {
+                                return false;
+                            }
+                            return (max === null || min === null);
+                        }
                     });
                 });
-
-                if (isLimit) {
-                    callback(dogs[layer], row, col);
-                }
-
             });
+
+            if (isLimit) {
+                callback(dogs, row, col);
+            }
+
         });
     });
+
 }
