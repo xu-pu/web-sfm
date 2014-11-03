93f91de1ada2a92e10f7694bc5ac7e51f3420509
ptx-pluto <sheep9275@gmail.com>
2014-11-04 02:03:19 +0800

    nn-search on kdtree finished

--- src/webregister/nn-search.js
+++ src/webregister/nn-search.js
@@ -1,8 +1,17 @@
 'use strict';
 
+var _ = require('underscore');
+
 module.exports = searchNN;
 
 
+/**
+ *
+ * @param {Node} root
+ * @param {Feature} feature
+ * @param {number} n
+ * @returns {MinimumQueue}
+ */
 function searchNN(root, feature, n){
     var mins = new MinimumQueue(n);
     searchTree(root, feature, mins);
@@ -78,9 +87,44 @@ function findLeaf(root, f){
  * @constructor
  */
 function MinimumQueue(n){
-
+    this.length = n;
+    this.queue = _.range(n).map(function(){
+        return { dist: Infinity, feature: null };
+    });
 }
 
-MinimumQueue.prototype.getMin = function(){};
 
-MinimumQueue.prototype.checkMin = function(){};
+/**
+ *
+ */
+MinimumQueue.prototype.getMin = function(){
+    return this.queue[this.length-1].dist;
+};
+
+
+/**
+ *
+ * @param {int} index
+ * @param {number} dist
+ */
+MinimumQueue.prototype.checkMin = function(index, dist){
+
+    var queue = this.queue,
+        length = this.length;
+
+    var cursor, offset;
+
+    for (cursor=0; cursor<length; cursor++) {
+        if (queue[cursor].dist > dist) {
+            break;
+        }
+    }
+
+    if (cursor < length) {
+        for (offset=length-cursor-1; offset>0; offset--) {
+            queue[cursor+offset] = queue[cursor+offset-1];
+        }
+        queue[cursor] = { dist: dist, feature: index };
+    }
+
+};
