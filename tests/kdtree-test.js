bef23ef0dec8353bf643190963d207eaea63ae27
ptx-pluto <sheep9275@gmail.com>
2014-11-04 23:40:28 +0800

    kd-tree and nn-search tested

--- tests/kdtree-test.js
+++ tests/kdtree-test.js
@@ -1,13 +1,26 @@
 'use strict';
 
 var kdtree = require('../src/webregister/kd-tree.js'),
-    Root = kdtree.Root,
+    searchNN = require('../src/webregister/nn-search.js'),
+    matchTree = require('../src/webregister/kdtree-matching.js'),
     samples = require('../src/utils/samples.js');
 
 
 function constructTree(i){
     var features = samples.getFeatures(i);
-    var root = new Root(features);
+    var root = kdtree.initTree(features);
+    console.log('kdtree generated');
+    var target = features[10];
+    var result = searchNN(root, target, 2);
+    console.log(result);
 }
 
-constructTree(8);
+function matchTest(i1, i2){
+    var features1 = samples.getFeatures(i1),
+        features2 = samples.getFeatures(i2);
+    matchTree(features1, features2);
+}
+
+//constructTree(8);
+
+matchTest(1,2);
