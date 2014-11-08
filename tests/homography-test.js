1629a7c58ebc7e5baa19b18125b4c3181612b46d
ptx-pluto <sheep9275@gmail.com>
2014-11-09 02:07:37 +0800

    draw homography fixed

--- tests/homography-test.js
+++ tests/homography-test.js
@@ -1,10 +1,9 @@
 'use strict';
 
-var Promise = require('promise'),
-    saveimage = require('save-pixels'),
-    grayscale = require('luminance'),
+var _ = require('underscore'),
+    Promise = require('promise'),
     fs = require('fs'),
-    lena = grayscale(require('lena')),
+    Canvas = require('canvas'),
     la = require('sylvester'),
     Matrix = la.Matrix,
     Vector = la.Vector;
@@ -12,16 +11,47 @@ var Promise = require('promise'),
 var samples = require('../src/utils/samples.js'),
     homography = require('../src/webmvs/homography.js'),
     rectification = require('../src/webmvs/rectification.js'),
-    testUtils = require('../src/utils/testing.js');
+    testUtils = require('../src/utils/testing.js'),
+    drawHomography = require('../src/visualization/drawHomography.js'),
+    projections = require('../src/math/projections.js'),
+    rotate = require('../src/math/rotate.js');
 
-var TEST_ANGEL = Math.PI/8;
+function testRotateMotion(i){
 
-var testRotation = Matrix.create([
-    [Math.cos(TEST_ANGEL), Math.sin(TEST_ANGEL), 0],
-    [-Math.sin(TEST_ANGEL), Math.cos(TEST_ANGEL), 0],
-    [0,0,1]
-]);
+    var width = 3008, height = 2000,
+        cam = samples.getCamera(i),
+        R = Matrix.create(cam.R),
+        t = Matrix.create(cam.t),
+        focal = cam.focal,
+        K = projections.getCalibrationMatrix(focal, width, height);
 
-var homoResult = homography(lena, testRotation);
+    cam.width = width;
+    cam.height = height;
 
-testUtils.promiseSaveNdarray(homoResult, '/home/sheep/Code/writetest1.png');
+    var RATIO = 0.25,
+        ROTATION = Math.PI/4,
+        STEPS = 10,
+        STEP = ROTATION/STEPS;
+
+    return samples
+        .promiseImage(i)
+        .then(function(img){
+            return Promise.all(_.range(STEPS+1).map(function(index){
+
+                var rotation = rotate(-index*STEP,index*STEP,index*STEP),
+                    H = K.x(rotation).x(K.inverse()),
+                    w = Math.round(width*RATIO),
+                    h = Math.round(height*RATIO),
+                    canv = new Canvas(w, h),
+                    ctx = canv.getContext('2d');
+
+                drawHomography(img, H, ctx, 0, 0, RATIO);
+
+                return testUtils.promiseWriteCanvas(canv, '/home/sheep/Code/HomoMotion' + index + '.png');
+
+            }));
+        });
+
+}
+
+testRotateMotion(5);
