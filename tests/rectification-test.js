55f16160e99a16f4dc1050fe94452a0161a157f6
ptx-pluto <sheep9275@gmail.com>
2014-11-15 16:38:38 +0800

    some tests

--- tests/rectification-test.js
+++ tests/rectification-test.js
@@ -51,59 +51,24 @@ function testPair(index1, index2){
             K1 = projections.getCalibrationMatrix(f1, width1, height1),
             K2 = projections.getCalibrationMatrix(f2, width2, height2);
 
-//        var H1 = K1.x(rotate(Math.PI/6, Math.PI/6, 0)).x(K1.inverse()),
-//            H2 = K2.x(rotate(Math.PI/8, Math.PI/6, Math.PI/5)).x(K2.inverse());
-
         var H1 = normalizeMatrix(K1.x(RR1).x(K1.inverse())),
             H2 = normalizeMatrix(K2.x(RR2).x(K2.inverse()));
+
+        console.log(RR1.x(RR1.transpose()));
+        console.log(RR2.x(RR2.transpose()));
 
         var F = projections.getFundamentalMatrix(R1, t1, f1, img1, R2, t2, f2, img2);
         var FF = projections.getFundamentalMatrix(R1new, t1new, f1, img1, R2new, t2new, f2, img2);
 
         var FFF = normalizeMatrix(H1.transpose().inverse().x(F).x(H2.inverse()));
 
-        console.log(FF.subtract(FFF));
-
         testUtils.promiseVisualEpipolar('/home/sheep/Code/before-rect.png', index1, index2, F);
         testUtils.promiseVisualEpipolar('/home/sheep/Code/after-rect.png', index1, index2, FF);
         testUtils.promiseVisualEpipolar('/home/sheep/Code/after-rect-homo.png', index1, index2, FFF);
 
-        //return testUtils.promiseVisualHomographyPiar('/home/sheep/Code/after-rect-homo.png', index1, index2, H1, H2);
+        return testUtils.promiseVisualHomographyPiar('/home/sheep/Code/result.png', index1, index2, H1, H2, FF);
 
     });
-
-}
-
-function rectificationTest(i1, i2){
-
-    var width = 3008, height = 2000,
-        f1 = samples.getCamera(i1).focal,
-        f2 = samples.getCamera(i2).focal,
-        cam = { width: width, height: height },
-        R1 = rotate(0, Math.PI/6, 0),
-        R2 = rotate(0, Math.PI/5, 0),
-        T1 = Vector.create([0,0,0]),
-        T2 = Vector.create([20,20,20]),
-        t1 = R1.x(T1).x(-1),
-        t2 = R2.x(T2).x(-1);
-
-    var results = rectification(R1, R2, t1, t2, f1, f2);
-    var RR1 = results[0],
-        RR2 = results[1],
-        R1new = RR1.x(R1), t1new = RR1.x(t1),
-        R2new = RR2.x(R2), t2new = RR2.x(t2);
-
-    //console.log(R1new);
-    //console.log(t1new);
-    //console.log(R2new);
-    //console.log(t2new);
-
-
-    var F = projections.getFundamentalMatrix(R1, t1, f1, cam, R2, t2, f2, cam),
-        FF = projections.getFundamentalMatrix(R1new, t1new, f1, cam, R2new, t2new, f2, cam);
-
-    testUtils.promiseVisualEpipolar('/home/sheep/Code/epipolar-line-before.png', i1, i2, F);
-    testUtils.promiseVisualEpipolar('/home/sheep/Code/epipolar-line-rectified.png', i1, i2, FF);
 
 }
 
@@ -115,4 +80,4 @@ function normalizeMatrix(m){
 
 //rectificationTest(6,7);
 
-testPair(6,7);
+testPair(18, 22);
