d24d3685aac3ed4319b06a72d21f6864145c8f9c
ptx-pluto <sheep9275@gmail.com>
2014-11-18 17:29:24 +0800

    line to rc more detailed

--- src/utils/cord.js
+++ src/utils/cord.js
@@ -31,7 +31,7 @@ module.exports.getRandomRT = getRandomRT;
  * @return {number[]}
  */
 function RCtoImg(row, col, cam){
-      return [col, cam.height-row-1, 1];
+      return [col, cam.height-row, 1];
 }
 
 
@@ -43,7 +43,7 @@ function RCtoImg(row, col, cam){
 function img2RT(point, height){
     point = point.x(1/point.e(3));
     return {
-        row: height-point.e(2)-1,
+        row: height-point.e(2),
         col: point.e(1)
     };
 }
@@ -51,35 +51,38 @@ function img2RT(point, height){
 //=======================================
 
 /**
- * @param {Feature} f
- * @param {Camera} cam
- * @returns {number[]}
- */
-function featureToImg(f, cam) {
-    return RCtoImg(f.row, f.col, cam);
-}
-
-
-/**
  *
  * @param line
  * @param {Camera} cam
  */
 function imgline2points(line, cam){
+
     var results = [],
         width = cam.width,
-        height = cam.height;
-    [   Vector.create([1,0,0]).cross(line),
-        Vector.create([1,0,-width]).cross(line),
-        Vector.create([0,1,-height]).cross(line),
-        Vector.create([0,1,0]).cross(line)
-    ].forEach(function(point){
-        var rt = rt2canvas(img2RT(point, height), width, height);
-        if (rt) {
-            results.push(rt);
-        }
-    });
+        height = cam.height,
+        p00 = Vector.create([0,0,1]),
+        p01 = Vector.create([0,height,1]),
+        p11 = Vector.create([width,height,1]),
+        p10 = Vector.create([width,0,1]);
+
+    [
+        p00.cross(p01),
+        p00.cross(p10),
+        p01.cross(p11),
+        p10.cross(p11)
+    ]
+        .map(function(edge){
+            return edge.cross(line)
+        })
+        .forEach(function(point){
+            var rt = rt2canvas(img2RT(point, height), width, height);
+            if (rt) {
+                results.push(rt);
+            }
+        });
+
     return results;
+
 }
 
 
@@ -93,6 +96,16 @@ function rt2canvas(rt, width, height){
     else {
         return null;
     }
+}
+
+
+/**
+ * @param {Feature} f
+ * @param {Camera} cam
+ * @returns {number[]}
+ */
+function featureToImg(f, cam) {
+    return RCtoImg(f.row, f.col, cam);
 }
 
 
