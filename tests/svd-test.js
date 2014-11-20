1b5dfbc6797a30f7ff120449703be8c7d6da5362
ptx-pluto <sheep9275@gmail.com>
2014-11-20 18:09:57 +0800

    svd test trimed

--- tests/svd-test.js
+++ tests/svd-test.js
@@ -4,18 +4,14 @@ var _ = require('underscore'),
     la = require('sylvester'),
     Matrix = la.Matrix,
     Vector = la.Vector,
-    Promise = require('promise'),
-    Canvas = require('canvas');
+    Promise = require('promise');
 
 var projections = require('../src/math/projections.js'),
     samples = require('../src/utils/samples.js'),
     testUtils = require('../src/utils/testing.js'),
     eightpoint = require('../src/webregister/eightpoint.js'),
     estimateFmatrix = require('../src/webregister/estimate-fmatrix.js'),
-    fmatrixError = eightpoint.fundamentalMatrixError,
-    drawImagePair = require('../src/visualization/drawImagePair.js'),
-    drawDetailedMatch = require('../src/visualization/drawDetailedMatch.js');
-
+    fmatrixError = eightpoint.fundamentalMatrixError;
 
 function testPair(i1, i2){
     var THRESHOLD = 5,
@@ -35,19 +31,14 @@ function testPair(i1, i2){
 
     console.log(filtered.length + '/' + matches.length + ' left.');
 
-//    var F = eightpoint(sampleSet, metadata);
     var sampleSet = _.sample(filtered, 100);
     var FF = estimateFmatrix(sampleSet, metadata);
 
     console.log(FF);
 
     return Promise.all([
-        testUtils.promiseVisualEpipolar('/home/sheep/Code/svd-fmatrix.png', i1, i2, fmatrix),
-        testUtils.promiseVisualEpipolar('/home/sheep/Code/svd-estimate-fmatrix1.png', i1, i2, FF),
-        testUtils.promiseDetailedMatches('/home/sheep/Code/svd-estimated-samples.png', i1, i2, sampleSet, FF),
-        testUtils.promiseDetailedMatches('/home/sheep/Code/svd-estimate-samples.png', i1, i2, sampleSet),
-        testUtils.promiseVisualMatch('/home/sheep/Code/svd-matches.png', i1, i2, matches),
-        testUtils.promiseVisualMatch('/home/sheep/Code/svd-matches-filtered.png', i1, i2, filtered)
+        testUtils.promiseDetailedMatches('/home/sheep/Code/svd-estimated.png', i1, i2, sampleSet, FF),
+        testUtils.promiseDetailedMatches('/home/sheep/Code/svd-reference.png', i1, i2, sampleSet)
     ]);
 
 }
