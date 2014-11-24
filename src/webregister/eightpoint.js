51b33596e5c571cb2cc52d775ded4adc598852d3
ptx-pluto <sheep9275@gmail.com>
2014-11-25 03:11:00 +0800

    eight point algorithm updated

--- src/webregister/eightpoint.js
+++ src/webregister/eightpoint.js
@@ -3,25 +3,30 @@
 var _ = require('underscore'),
     la = require('sylvester'),
     Matrix = la.Matrix,
-    Vector = la.Vector,
-    numeric = require('numeric'),
-    cord = require('../utils/cord.js');
+    Vector = la.Vector;
+
+var ransac = require('./ransac.js'),
+    cord = require('../utils/cord.js'),
+    estimateFmatrix = require('./estimate-fmatrix.js');
 
 //====================================================
 
+
 /**
+ * @typedef {{features1: Feature[], features2: Feature[], cam2: Camera, cam2: Camera}} TwoViewMetadata
+ */
+
+
+//====================================================
+
+
+/**
+ *
  * @param {int[][]} matches
- * @param {object} metadata
- * @param metadata.cam1
- * @param metadata.cam2
- * @param {Feature[]} metadata.features1
- * @param {Feature[]} metadata.features2
+ * @param {TwoViewMetadata} metadata
+ * @returns {{dataset: [], F}}
  */
 module.exports = function(matches, metadata){
-
-    if (matches.length !== 8){
-        throw 'need exact 8 points';
-    }
 
     var cam1 = metadata.cam1,
         cam2 = metadata.cam2,
@@ -29,59 +34,61 @@ module.exports = function(matches, metadata){
         features2 = metadata.features2;
 
     var T1 = Matrix.create([
-        [2.0/cam1.width, 0,               -1],
-        [0,              2.0/cam1.height, -1],
-        [0,              0,                1]
+        [ 1/cam1.width, 0            , 0 ],
+        [ 0           , 1/cam1.height, 0 ],
+        [ 0           , 0            , 1 ]
     ]);
 
     var T2 = Matrix.create([
-        [2.0/cam2.width, 0,               -1],
-        [0,              2.0/cam2.height, -1],
-        [0,              0,                1]]
-    );
-
-    var A = Matrix.create(_.map(matches, function(match){
-        var f1 = features1[match[0]],
-            f2 = features2[match[1]];
-        var p1 = Matrix.create([cord.featureToImg(f1, cam1)]).transpose(),
-            p2 = Matrix.create([cord.featureToImg(f2, cam2)]).transpose();
-        p1 = T1.x(p1);
-        p2 = T2.x(p2);
-        return _.flatten(p2.x(p1.transpose()).elements);
-    }));
-
-    var result = Matrix.create(numeric.svd(A.transpose().elements).U).col(8);
-
-    var F = Matrix.create([
-        result.elements.slice(0, 3),
-        result.elements.slice(3, 6),
-        result.elements.slice(6, 9)
+        [ 1/cam2.width, 0            , 0 ],
+        [ 0           , 1/cam2.height, 0 ],
+        [ 0           , 0            , 1 ]
     ]);
 
-    if (F.determinant() !== 0) {
-        var fSVD = F.svd();
-        fSVD.S.elements[2][2] = 0;
-        F = fSVD.U.x(fSVD.S).x(fSVD.V.transpose());
-    }
-    return T1.transpose().x(F).x(T2);
+    var normalizedMatches = matches.map(function(match){
+        var f1 = features1[match[0]],
+            f2 = features2[match[1]],
+            p1 = Vector.create(cord.featureToImg(f1, cam1)),
+            p2 = Vector.create(cord.featureToImg(f2, cam2));
+        return { x1: T1.x(p1), x2: T2.x(p2) };
+    });
+
+    var results = ransac({
+        dataset: normalizedMatches,
+        metadata: null,
+        subset: 8,
+        relGenerator: estimateFmatrix,
+        errorGenerator: module.exports.fundamentalMatrixError,
+        outlierThreshold: 0.2,
+        errorThreshold: 0.006,
+        trials: 1000
+    });
+
+    var Fsvd = estimateFmatrix(_.sample(results.dataset, 100)),
+        F = refineF(Fsvd, results.dataset);
+
+    F = T1.transpose().x(F).x(T2);
+
+    return {
+        dataset: results.dataset,
+        F: F
+    };
+
 };
+
+function refineF(F, matches){
+    return F;
+}
 
 
 /**
  * @param F
- * @param {int[]} match
- * @param metadata
- * @param {Camera} metadata.cam1
- * @param {Camera} metadata.cam2
- * @param {Feature[]} metadata.features1
- * @param {Feature[]} metadata.features2
+ * @param {{x1, x2}} match
  * @return {number}
  */
-module.exports.fundamentalMatrixError = function(F, match, metadata){
-    var f1 = metadata.features1[match[0]],
-        f2 = metadata.features2[match[1]],
-        p1 = Vector.create(cord.featureToImg(f1, metadata.cam1)),
-        p2 = Vector.create(cord.featureToImg(f2, metadata.cam2)),
+module.exports.fundamentalMatrixError = function(F, match){
+    var p1 = match.x1,
+        p2 = match.x2,
         line = F.x(p2),
         a = line.e(1), b = line.e(2),
         modulus = Math.sqrt(a*a+b*b);
