357f35948880736c48f5ab7c6e26ddae2f4dd29b
ptx-pluto <sheep9275@gmail.com>
2014-11-25 17:08:30 +0800

    deleted lagacy files, rectification reformated

--- src/webmvs/rectification.js
+++ src/webmvs/rectification.js
@@ -1,16 +1,21 @@
 'use strict';
 
-/**
- * @typedef {{R, t}} CalibratedCamera
- */
-
 var la = require('sylvester'),
     Matrix = la.Matrix,
     Vector = la.Vector;
 
-module.exports = rectification;
+//===================================================
 
-function rectification(R1, R2, t1, t2){
+
+/**
+ * Rectify a pair of calibrated cameras
+ * @param R1
+ * @param R2
+ * @param t1
+ * @param t2
+ * @returns {[]} - rotations needed for rectification of each camera
+ */
+module.exports = function(R1, R2, t1, t2){
     var R = R2.x(R1.transpose()),
         t = t2.subtract(R.x(t1)),
         T = R.transpose().x(t).x(-1),
@@ -19,7 +24,11 @@ function rectification(R1, R2, t1, t2){
         e3 = normalize(e1.cross(e2)),
         rect = Matrix.create([e1.elements, e2.elements, e3.elements]);
     return [rect, rect.x(R.transpose())]; // delta R1, R2
-}
+};
+
+
+//===================================================
+
 
 function normalize(v){
     return v.multiply(1/v.modulus())
