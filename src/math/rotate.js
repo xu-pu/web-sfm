43b8178dfa9d9a50675fdbe97d3f1691330d5318
ptx-pluto <sheep9275@gmail.com>
2014-11-27 15:18:47 +0800

    rotate modified

--- src/math/rotate.js
+++ src/math/rotate.js
@@ -2,14 +2,34 @@
 
 var la = require('sylvester'),
     Matrix = la.Matrix,
-    Vector = la.Vector;
+    Vector = la.Vector,
+    Plane = la.Plane;
 
-module.exports = getRotation;
 
-
-function getRotation(x,y,z){
+module.exports.getRotation = function getRotation(x,y,z){
     return rotateX(x).x(rotateY(y)).x(rotateZ(z));
-}
+};
+
+
+/**
+ * Convert rotation matrix to euler angles
+ * @param R
+ * @returns {number[]}
+ */
+module.exports.toEuler = function(R){
+
+    var origin = Vector.create([0,0,0]),
+        z = Vector.create([0,0,1]),
+        Z = R.x(z),
+        p = Plane.create(origin, z),
+        P = Plane.create(origin, Z),
+        nline = p.intersectionWith(P).direction,
+        n = nline.x(1/nline.modulus()),
+        N = R.transpose().x(n);
+
+
+
+};
 
 
 function rotateX(angle){
