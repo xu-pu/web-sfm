dc453567ac03d8763acd6a92496e3e23d3edfdda
ptx-pluto <sheep9275@gmail.com>
2014-11-28 00:30:34 +0800

    maching basics added

--- src/webmvs/phase-match.js
+++ src/webmvs/phase-match.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var _ = require('underscore'),
+    la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector;
+
+var triangulation = require('../webregister/triangulation.js'),
+    Patch = require('./Patch.js');
+
+//===============================================================
+
 
 /**
  * The match phase in PMVS
@@ -7,3 +17,47 @@
 module.exports = function(){
 
 };
+
+
+/**
+ *
+ * @param {PmvsState} state
+ * @param {ImageCellGrid} imgRef
+ * @param {ImageCellGrid} img
+ * @param {Vector} fRef
+ * @param {Vector} f
+ * @returns {Patch}
+ */
+function patchFromFeaturePair(state, imgRef, img, fRef, f){
+
+
+    var images = state.images,
+        R = imgRef;
+
+    var initC = triangulation(imgRef.P, img.P, fRef, f);
+    var initN;
+
+    var initV = images.filter(function(img){
+
+    });
+
+    var initVstar = initV.filter(function(img){
+
+    });
+
+    var nc = refinePatch(initC, initN, initV, initVstar);
+
+    var n = nc.n,
+        c = nc.c;
+
+    var V = images.filter(function(img){
+
+    });
+
+    var Vstar = initV.filter(function(img){
+
+    });
+
+    return new Patch({ c: c, n: n, R: R, V: V, Vstar: Vstar });
+
+}
--- src/webmvs/pmvs.js
+++ src/webmvs/pmvs.js
@@ -1,6 +1,11 @@
 'use strict';
 
 /**
+ * @typedef {{images: ImageCellGrid[], patches: Patch[]}} PmvsState
+ */
+
+
+/**
  * PMVS - patch based Multi-View Stereo
  */
 module.exports = function(){
