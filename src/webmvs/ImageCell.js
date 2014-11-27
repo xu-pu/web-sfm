e8233447152281b017046834b173cb6ddda6867b
ptx-pluto <sheep9275@gmail.com>
2014-11-27 20:41:43 +0800

    search features along epipolar line added

--- src/webmvs/ImageCell.js
+++ src/webmvs/ImageCell.js
@@ -1,10 +1,73 @@
 'use strict';
 
+var _ = require('underscore'),
+    la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector;
+
+var MVS_SETTINGS = require('./settings.js'),
+    cord = require('../utils/cord.js'),
+    geoUtils = require('../math/geometry-utils.js');
+
+//==========================================================
+
+module.exports = ImageCell;
+
+//==========================================================
+
 
 /**
  * Image cell class
+ *
+ * @property {undefined|Feature[]} features
+ * @property {ImageCellGrid} grid
+ * @property {int} row
+ * @property {int} col
  * @constructor
  */
-module.exports = function(){
+function ImageCell(grid, row, col){
+
+    _.extend(this, {
+        grid: grid,
+        row: row,
+        col: col
+    });
+
+}
+
+
+/**
+ * Search for features match the epipolar line in the cell
+ * @param line
+ * @returns {Boolean|[]}
+ */
+ImageCell.prototype.searchEpipolarLine = function(line){
+
+    var BOUND = MVS_SETTINGS.EPIPOLAR_LINE_RADIUS;
+
+    if (!this.features) {
+        // features removed from cell
+        return false;
+    }
+
+    var mu = this.grid.mu,
+        center = Vector.create(cord.RCtoImg(mu*this.row+mu/2, mu*this.col+mu/2, this.grid.cam));
+
+    if (geoUtils.getPoint2Line(center, line) > this.grid.bound) {
+        // out of radius
+        return false;
+    }
+
+    var selected = this.features.filter(function(point){
+        return geoUtils.getPoint2Line(point, line) <= BOUND;
+    });
+
+    if (selected.length === 0) {
+        // no feature matched the epipolar line
+        return false;
+    }
+    else {
+        return selected;
+    }
 
 };
