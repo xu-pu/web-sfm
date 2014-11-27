0e3562b4b6990df77dfaa838464dff04eb884737
ptx-pluto <sheep9275@gmail.com>
2014-11-27 21:08:21 +0800

    RANSAC refactored

--- src/webregister/ransac.js
+++ src/webregister/ransac.js
@@ -1,31 +1,48 @@
+'use strict';
+
 var _ = require('underscore');
 
-module.exports = ransac;
+//===============================================================================
+
 
 /**
- * @param {Object[]} options.dataset
- * @param {number} options.outlierThreshold
- * @param {int} options.trials
- * @param options.metadata -- passed to the relGenerator
- * @param {function} options.relGenerator
- * @param {function} options.errorGenerator
- * @param {number} options.errorThreshold
- * @param {int} options.subset
+ * @param {[]} options.dataset
+ * @param {number}   options.outlierThreshold
+ * @param {int}      options.trials
+ * @param            options.metadata
+ * @param {function} options.relGenerator   -- (subset, metadata) => rel
+ * @param {function} options.errorGenerator -- (rel, entry, metadata) => error
+ * @param {number}   options.errorThreshold
+ * @param {int}      options.subset
  * @return {{dataset: [], rel}}
  */
-function ransac(options){
-    var relEsitmate, inliers, trials=options.trials;
+module.exports = function(options){
+
+    var dataset           = options.dataset,
+        metadata          = options.metadata,
+        errorGenerator    = options.errorGenerator,
+        relGenerator      = options.relGenerator,
+        OUTLIER_THRESHOLD = options.outlierThreshold,
+        ERROR_THRESHOLD   = options.errorThreshold,
+        SUBSET            = options.subset,
+        TRIALS            = options.trials;
+
+    var relEsitmate, inliers,
+        trials=TRIALS;
+
     while(trials !== 0){
-        relEsitmate = options.relGenerator(_.sample(options.dataset, options.subset), options.metadata);
-        inliers = _.filter(options.dataset, function(m){
-            return options.errorGenerator(relEsitmate, m, options.metadata) < options.errorThreshold;
+        relEsitmate = relGenerator(_.sample(dataset, SUBSET), metadata);
+        inliers = dataset.filter(function(entry){
+            return errorGenerator(relEsitmate, entry, metadata) < ERROR_THRESHOLD;
         });
-        if (inliers.length/options.dataset.length >= 1-options.outlierThreshold) {
-            console.log('Success, ' + inliers.length+'/'+options.dataset.length + ' passed RANSAC after ' + (options.trials-trials) + ' trials');
+        if (inliers.length/dataset.length >= 1-OUTLIER_THRESHOLD) {
+            console.log('Success, ' + inliers.length + '/' + dataset.length + ' passed RANSAC after ' + (TRIALS-trials) + ' trials');
             return { dataset: inliers, rel: relEsitmate };
         }
-        console.log(inliers.length+'/'+options.dataset.length+ ', ' + (100*inliers.length/options.dataset.length) + '% passed RANSAC, trial failed');
+        console.log(inliers.length + '/' + dataset.length + ', ' + (100*inliers.length/dataset.length) + '% passed RANSAC, trial failed');
         trials--;
     }
+
     throw "RANSAC faild";
-}
+
+};
