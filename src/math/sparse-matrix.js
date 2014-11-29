8b342c54f81fb91632b0615a8e4b8d47d6feea50
ptx-pluto <sheep9275@gmail.com>
2014-11-29 16:19:36 +0800

    sparse matrix declared

---
+++ src/math/sparse-matrix.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.SparseMatrix = SparseMatrix;
+module.exports.SparseMatrixBuilder = SparseMatrixBuilder;
+
+
+/**
+ * Column compressed sparse matrix
+ *
+ * @property {boolean} isSparse
+ *
+ * @constructor
+ */
+function SparseMatrix(){
+
+}
+
+
+function SparseMatrixBuilder(){
+
+}
