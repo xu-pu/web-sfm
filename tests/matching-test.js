6c43bf122d4fa0b9537a498bedaf197cc5a1c692
ptx-pluto <sheep9275@gmail.com>
2014-12-06 16:24:11 +0800

    feature match test passed

--- tests/matching-test.js
+++ tests/matching-test.js
@@ -6,26 +6,24 @@ var _ = require('underscore'),
     Vector = la.Vector,
     Promise = require('promise');
 
-var kdtree = require('../src/webmatcher/kd-tree.js'),
-    searcher = require('../src/webmatcher/search-ann.js'),
+var matcher = require('../src/webmatcher/match-feature.js'),
     samples = require('../src/utils/samples.js'),
-    utils = require('../src/utils/image-conversion.js'),
+    testUtils = require('../src/utils/testing.js'),
     cord = require('../src/utils/cord.js');
+
+
 
 
 function testPiar(i1, i2){
 
     var features1 = samples.getFeatures(i1),
-        features2 = samples.getFeatures(i2),
-        kdtree1 = kdtree.initTree(features1),
-        kdtree2 = kdtree.initTree(features2);
+        features2 = samples.getFeatures(i2);
 
-    features1.forEach(function(f1){
-        var results = searcher.searchANN(kdtree2, f1, 2, 0.3);
-        //var results = searcher.searchNN(kdtree2, f1, 2);
-        console.log('one done');
-    });
+    var matches = matcher(features1, features2);
 
+    return Promise.all([
+        testUtils.promiseVisualMatch('/home/sheep/Code/matching-ann.png', i1, i2, matches)
+    ]);
 
 }
 
