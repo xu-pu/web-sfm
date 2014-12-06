52b0a75c6297801c1ae5840588f8fee1e9c5f326
ptx-pluto <sheep9275@gmail.com>
2014-12-06 16:11:55 +0800

    ann feature matching added

--- src/webmatcher/match-feature.js
+++ src/webmatcher/match-feature.js
@@ -8,63 +8,90 @@ var kdtree = require('./kd-tree.js'),
 //=============================================================
 
 
+var ANN_ERROR_THRESHOLD = 0.1,
+    ANN_THRESHOLD = 0.6,
+    ANN_SQUARE = ANN_THRESHOLD*ANN_THRESHOLD;
+
+
+//=============================================================
+
+
 /**
- *
+ * Approximate Nearest Neighbor matching for SIFT features
  * @param {Feature[]} features1
  * @param {Feature[]} features2
  * @returns {number[][]}
  */
 module.exports = function(features1, features2){
 
-    var ff1 = features1.map(function(f){ return f; }),
-        ff2 = features2.map(function(f){ return f; }),
+    var ff1 = features1,
+        ff2 = features2,
         tree1 = kdtree.initTree(ff1),
         tree2 = kdtree.initTree(ff2);
 
-    var matches = [], backMatches = [];
-    ff1.forEach(function(f, i){
-        console.log('match one');
-        var f2 = findNNFeature(tree2, f);
-        if (f2 !== null) {
-            var f1;
-            if (_.isNumber(backMatches[f2])) {
-                f1 = backMatches[f2];
+    var matches = [],
+        backMatches = []; // fi2 -> fi1
+
+    tree1.features.forEach(function(f, i){
+
+        var fi1, queue1,
+            fi2, queue2;
+
+        queue2 = searcher.searchANN(tree2, f, ANN_ERROR_THRESHOLD);
+
+        if (isMatchValid(queue2)) {
+
+            fi2 = queue2.optimal.feature;
+
+            if (backMatches[fi2] === -1) {
+                return;
+            }
+            else if (_.isNumber(backMatches[fi2])) {
+                fi1 = backMatches[fi2];
             }
             else {
-                f1 = findNNFeature(tree1, ff2[f2]);
-                if (f1 !== null) {
-                    backMatches[f2] = f1;
+                queue1 = searcher.searchANN(tree1, tree2.features[fi2], ANN_ERROR_THRESHOLD);
+                if (isMatchValid(queue1)) {
+                    fi1 = queue1.optimal.feature;
+                    backMatches[fi2] = fi1;
+                }
+                else {
+                    backMatches[fi2] = -1;
+                    return;
                 }
             }
-            if (f1 === i) {
-                matches.push([f1, f2]);
+
+            if (fi1 === i) {
+                matches.push([fi1, fi2]);
+                console.log(matches.length + 'th found at ' + i + '/'+ features1.length);
             }
+
         }
+
     });
 
     return matches.map(function(pair){
+
+        var fi1 = pair[0],
+            fi2 = pair[1],
+            f1 = tree1.features[fi1],
+            f2 = tree2.features[fi2];
+
         return [
-            features1.indexOf(ff1[pair[0]]),
-            features2.indexOf(ff2[pair[1]])
+            features1.indexOf(f1),
+            features2.indexOf(f2)
         ];
+
     });
 
 };
 
 
 /**
- *
- * @param {KdtreeNode} tree
- * @param {Feature} feature
- * @returns {number|null}
+ * Do the minimum queue pass the ANN_THRESHOLD
+ * @param {BinaryMinimumQueue} results
+ * @returns {boolean}
  */
-function findNNFeature(tree, feature){
-    var mins = searchNN(tree, feature, 2),
-        queue = mins.queue;
-    if (queue[0].dist/queue[1].dist < 0.6*0.6) {
-        return queue[0].feature;
-    }
-    else {
-        return null;
-    }
+function isMatchValid(results){
+    return results.optimal.dist/results.second.dist < ANN_SQUARE;
 }
