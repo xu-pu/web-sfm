64ba3d736d5b8e28ed01fb7dd6cbf12803eef9d0
ptx-pluto <sheep9275@gmail.com>
2014-12-07 02:55:11 +0800

    tracking finished

--- src/webmatcher/tracking.js
+++ src/webmatcher/tracking.js
@@ -4,19 +4,24 @@ var _ = require('underscore');
 
 //======================================================
 
+var connectivity = require('./connectivity.js'),
+    ConnectivityGraph = connectivity.ConnectivityGraph;
+
+//======================================================
+
 
 /**
- * @typedef {{cam: number, point: number}} View
+ * @typedef {{ cam: number, point: number }} TrackView
  */
 
 
 /**
- * @typedef {Object} Track - cam->feature
+ * @typedef {TrackView[]} Track
  */
 
 
 /**
- * @typedef {{cam1: number, cam2: number, matches: number[][]}} TwoViewMatches
+ * @typedef {{ cam1: number, cam2: number, matches: number[][] }} TwoViewMatches
  */
 
 
@@ -24,9 +29,46 @@ var _ = require('underscore');
 
 
 /**
- * @param {TwoViewMatches[]} matchLists
- * @returns {{tracks:Track[], views: Object}}
+ * @param {TwoViewMatches[]} dataset
+ * @returns {{ tracks:Track[], views: Track[][] }} - views[cam]=>Track[]
  */
-module.exports = function(matchLists) {
+module.exports = function(dataset) {
+
+    var graph = new ConnectivityGraph(dataset),
+        closures = graph.getTracks(),
+        tracks = [],
+        views = [];
+
+    closures.forEach(function(nodes){
+
+        var cams = [], track = [];
+
+        var trackValid = nodes.every(function(node){
+            if (cams.indexOf(node.cam) === -1) {
+                cams.push(node.cam);
+                track.push({ cam: node.cam, point: node.feature });
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+
+        if (trackValid) {
+            tracks.push(track);
+            cams.forEach(function(cam){
+                var view = views[cam];
+                if (view) {
+                    view.push(track);
+                }
+                else {
+                    views[cam] = [track];
+                }
+            });
+        }
+
+    });
+
+    return { tracks: tracks, views: views };
 
 };
