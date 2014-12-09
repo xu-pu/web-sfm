fbaafe69c4b1709f1eff9f42e33b0d3fdcff7f1b
ptx-pluto <sheep9275@gmail.com>
2014-12-10 02:27:35 +0800

    PMVS Context with parameters

--- src/webmvs/pmvs-context.js
+++ src/webmvs/pmvs-context.js
@@ -4,17 +4,26 @@ module.exports = PMVSContext;
 
 
 /**
- * @params {ImageCellGrid[]} images
- * @params {Patch[]} patches
+ * @param {ImageCellGrid[]} images
+ * @param {Patch[]} patches
+ * @param {int} beta1
+ * @param {int} beta2
+ * @param {int} mu
  *
  * @property {ImageCellGrid[]} images
  * @property {Patch[]} patches
+ * @property {int} beta1
+ * @property {int} beta2
+ * @property {int} mu
  *
  * @constructor
  */
-function PMVSContext(images, patches){
+function PMVSContext(images, patches, beta1, beta2, mu){
     this.images = images;
     this.patches = patches;
+    this.beta1 = beta1;
+    this.beta2 = beta2;
+    this.mu = mu;
 }
 
 
