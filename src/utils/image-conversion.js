39ce6ed8f09c38cc01c08bc2b267d7905491ad29
ptx-pluto <sheep9275@gmail.com>
2014-12-20 01:59:17 +0800

    gray2rgb moved

--- src/utils/image-conversion.js
+++ src/utils/image-conversion.js
@@ -37,3 +37,24 @@ module.exports.getDownsample = function(img){
     downsample(result, img, 0, 255);
     return result;
 };
+
+
+/**
+ * Grayscale to RGB ndarray
+ * @param img
+ * @returns {*}
+ */
+module.exports.gray2rgb = function(img){
+    var width = img.shape[1],
+        height = img.shape[0],
+        result = pool.malloc([height, width, 3]);
+    var row, col;
+    for (row=0; row<height; row++) {
+        for (col=0; col<width; col++) {
+            result.set(row, col, 0, img.get(row, col));
+            result.set(row, col, 1, img.get(row, col));
+            result.set(row, col, 2, img.get(row, col));
+        }
+    }
+    return result;
+};
