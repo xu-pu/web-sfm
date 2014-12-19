ea54a9d5984bbe774aaf24b01040ed18047badbc
ptx-pluto <sheep9275@gmail.com>
2014-12-20 01:28:10 +0800

    downsample added

--- src/utils/image-conversion.js
+++ src/utils/image-conversion.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var ndarray = require('ndarray');
+var ndarray = require('ndarray'),
+    pool = require('ndarray-scratch'),
+    downsample = require('ndarray-downsample2x');
 
 //======================================================
 
@@ -23,4 +25,15 @@ module.exports.imagedata2ndarray = function(imagedata){
 module.exports.canvas2ndarray = function(ctx, width, height){
     var imagedata = ctx.getImageData(0, 0, width, height);
     return exports.imagedata2ndarray(imagedata);
+};
+
+
+/**
+ * Downsample ndarray
+ * @param img
+ */
+module.exports.getDownsample = function(img){
+    var result = pool.malloc(img.step(2,2).shape);
+    downsample(result, img, 0, 255);
+    return result;
 };
