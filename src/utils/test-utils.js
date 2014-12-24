b3de7bb10fdc52a648366e98152198f1993ef9ac
ptx-pluto <sheep9275@gmail.com>
2014-12-25 00:59:01 +0800

    basic testing utils added

---
+++ src/utils/test-utils.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var _ = require('underscore'),
+    la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector,
+    Canvas = require('canvas'),
+    Promise = require('promise'),
+    fs = require('fs'),
+    saveimage = require('save-pixels'),
+    getPixels = require('get-pixels'),
+    grayscale = require('luminance'),
+    Image = require('canvas').Image;
+
+//=====================================================
+
+
+/**
+ *
+ * @param {string} path
+ * @returns {Promise}
+ */
+module.exports.promiseImage = function(path){
+    return new Promise(function(resolve, reject){
+        getPixels(path, function(err, img){
+            console.log('image loaded');
+            resolve(grayscale(img));
+        });
+    });
+};
+
+
+/**
+ *
+ * @param {string} path
+ * @returns {Promise}
+ */
+module.exports.promiseCanvasImage = function(path){
+    return new Promise(function(resolve, reject){
+        fs.readFile(path, function(err, buffer){
+            if (err) {
+                console.log('load failed');
+                reject(err);
+            }
+            var img = new Image;
+            img.src = buffer;
+            resolve(img);
+        });
+    });
+};
+
+
+//=====================================================
+// Data saving utilities
+//=====================================================
+
+
+/**
+ * @param {string} path
+ * @param img
+ */
+module.exports.promiseSaveNdarray = function(path, img){
+    var writeStream = fs.createWriteStream(path);
+    saveimage(img, 'png').pipe(writeStream);
+};
+
+
+/**
+ *
+ * @param {string} path
+ * @param {ArrayBuffer} buffer
+ * @returns {Promise}
+ */
+module.exports.promiseWriteFile = function(path, buffer){
+    return new Promise(function(resolve, reject){
+        fs.writeFile(path, buffer, function(err){
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
+    });
+};
+
+
+/**
+ *
+ * @param {Canvas} canvas
+ * @param {string} path
+ * @returns {Promise}
+ */
+module.exports.promiseWriteCanvas = function(path, canvas){
+    return exports.promiseWriteFile(path, canvas.toBuffer());
+};
+
+
+/**
+ * Save json to file
+ * @param {string} path
+ * @param {Object} obj
+ * @returns {Promise}
+ */
+module.exports.promiseSaveJson = function(path, obj){
+    return exports.promiseWriteFile(path, JSON.stringify(obj));
+};
