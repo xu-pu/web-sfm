377217d511d45f3e6b9f25fa32c263877517d43f
ptx-pluto <sheep9275@gmail.com>
2014-12-29 17:24:04 +0800

    demos config file refactorized

---
+++ demo/demos.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = [
+
+    {
+
+        name: 'Hall-Demo',
+
+        description: require('./Hall-Demo/description.json')
+
+    },
+
+    {
+
+        name: 'Another-Hall-Demo',
+
+        description: require('./Hall-Demo/description2.json')
+
+    }
+
+];
