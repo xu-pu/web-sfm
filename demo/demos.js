0e5ccfe33d998eb4586ee0ca46805d03a640a712
ptx-pluto <sheep9275@gmail.com>
2015-01-07 02:11:05 +0800

    new demo added

--- demo/demos.js
+++ demo/demos.js
@@ -12,9 +12,9 @@ module.exports = [
 
     {
 
-        name: 'Another-Hall-Demo',
+        name: 'Leuven-City-Hall-Demo',
 
-        description: require('./Hall-Demo/description2.json')
+        description: require('./Leuven-City-Hall-Demo/description.json')
 
     }
 
