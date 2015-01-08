af9c35f00f1141da9f005e01c9835c4c45a460a7
ptx-pluto <sheep9275@gmail.com>
2015-01-08 23:29:39 +0800

    image calculus

---
+++ src/math/image-calculus.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * @typedef {{ mag: number, ori: number }} Gradient
+ */
+
+
+/**
+ *
+ * @param img
+ * @param {int} x
+ * @param {int} y
+ * @returns {Gradient}
+ */
+module.exports.discreteGradient = function(img, x, y){
+    var dx = exports.discreteDx(img, x, y),
+        dy = exports.discreteDy(img, x, y),
+        mag = Math.sqrt(dx*dx+dy*dy),
+        ori = Math.atan2(dy, dx);
+    return { mag: mag, ori: ori };
+};
+
+
+/**
+ *
+ * @param img
+ * @param {int} x
+ * @param {int} y
+ * @returns {number}
+ */
+module.exports.discreteDx = function(img, x, y) {
+    return img.get(x+1, y) - img.get(x-1, y);
+};
+
+
+/**
+ *
+ * @param img
+ * @param {int} x
+ * @param {int} y
+ * @returns {number}
+ */
+module.exports.discreteDxx = function(img, x, y){
+
+};
+
+
+/**
+ *
+ * @param img
+ * @param {int} x
+ * @param {int} y
+ * @returns {number}
+ */
+module.exports.discreteDy = function(img, x, y){
+    return img.get(x, y-1) - img.get(x, y+1);
+};
+
+
+/**
+ *
+ * @param img
+ * @param {int} x
+ * @param {int} y
+ * @returns {number}
+ */
+module.exports.discreteDyy = function(img, x, y){
+
+};
