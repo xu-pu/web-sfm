c3eabf3e26342f03ffb77ea95d743f97b676eafc
ptx-pluto <sheep9275@gmail.com>
2015-01-13 23:05:00 +0800

    zoom added

--- src/app/RouteViews/WorkspaceMatcherView.js
+++ src/app/RouteViews/WorkspaceMatcherView.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var GRID_SIZE_MAX = 2500,
+    GRID_SIZE_MIN = 500;
+
 module.exports = Ember.View.extend({
 
     tagName: 'div',
@@ -7,6 +10,23 @@ module.exports = Ember.View.extend({
     classNames: [
         'main-container__match-grid'
     ],
+
+    registerWheel: function(){
+        var _self = this;
+        jQuery(this.get('element')).on('wheel', function(e){
+            _self.wheel(e.originalEvent);
+        });
+    }.on('didInsertElement'),
+
+    wheel: function(e){
+        var $grid = jQuery('.match-grid__body'),
+            currentSize = $grid.height(),
+            speed = 0.02,
+            nextSize = currentSize * (e.deltaY < 0 ? 1+speed : 1-speed);
+        if (nextSize > GRID_SIZE_MIN && nextSize < GRID_SIZE_MAX) {
+            $grid.css('width', nextSize).css('height', nextSize);
+        }
+    },
 
     GridView: Ember.View.extend({
 
