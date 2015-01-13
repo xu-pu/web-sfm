20f049fbfb33d26f41cf80612411ee16e3aca855
ptx-pluto <sheep9275@gmail.com>
2015-01-13 22:17:46 +0800

    match grid hover added

--- src/app/RouteViews/WorkspaceMatcherView.js
+++ src/app/RouteViews/WorkspaceMatcherView.js
@@ -8,63 +8,79 @@ module.exports = Ember.View.extend({
         'main-container__match-grid'
     ],
 
-    hover: null,
-
-    isTraced: function(){
-        if (this.get('hover') === null) {
-            return false;
-        }
-        else {
-            return !this.get('hover.isDiag');
-        }
-    }.property('hover'),
-
-    NodeView: Ember.View.extend({
+    GridView: Ember.View.extend({
 
         tagName: 'div',
 
-        from: null,
+        classNames: ['match-grid__body'],
 
-        to: null,
+        hover: null,
 
-        mouseEnter: function(){
-            this.get('parentView').set('hover', this);
+        mouseLeave: function(){
+            this.set('hover', null);
         },
 
-        /*
-        click: function(){
-            this.get('controller').transitionToRoute('matcher.pair', { from: this.get('from'), to: this.get('to') });
-        },
-        */
+        NodeView: Ember.View.extend({
 
-        isTrace: function(){
-            var hover = this.get('parentView.hover');
-            if (!hover.get('isDiag')) {
-                return this.get('from') === hover.get('from') || this.get('to') === hover.get('to');
-            }
-            else {
-                return false;
-            }
-        }.property('parentView.hover'),
+            tagName: 'div',
 
-        isFinished: function(){
-            return !this.get('isDiag') && this.get('controller.finished').indexOf(this.get('key')) !== -1;
-        }.property('controller.matches.length', 'from', 'to'),
+            classNames: ['match-grid__node'],
 
-        isDiag: function(){
-            return this.get('from') === this.get('to');
-        }.property('from', 'to')
+            classNameBindings: ['isDiag', 'isTrace', 'isHover'],
 
-        /*
-        isInprogress: function(){
-            if (this.get('controller.scheduler')) {
-                return this.get('controller.scheduler.inProgress').indexOf(this.get('key')) !== -1;
-            }
-            else {
-                return false;
-            }
-        }.property('controller.scheduler.inProgress.length')
-        */
+            from: null,
+
+            to: null,
+
+            mouseEnter: function(){
+                if (this.get('isDiag')) {
+                    this.get('parentView').set('hover', null);
+                }
+                else {
+                    this.get('parentView').set('hover', this);
+                }
+            },
+
+            /*
+             click: function(){
+             this.get('controller').transitionToRoute('matcher.pair', { from: this.get('from'), to: this.get('to') });
+             },
+             */
+
+            isTrace: function(){
+                var hover = this.get('parentView.hover');
+                if (!!hover && !hover.get('isDiag') && this!=hover) {
+                    return this.get('from') === hover.get('from') || this.get('to') === hover.get('to');
+                }
+                else {
+                    return false;
+                }
+            }.property('parentView.hover'),
+
+            isFinished: function(){
+                return !this.get('isDiag') && this.get('controller.finished').indexOf(this.get('key')) !== -1;
+            }.property('controller.matches.length', 'from', 'to'),
+
+            isDiag: function(){
+                return this.get('from') === this.get('to');
+            }.property('from', 'to'),
+
+            isHover: function(){
+                return this === this.get('parentView.hover');
+            }.property('parentView.hover')
+
+            /*
+             isInprogress: function(){
+             if (this.get('controller.scheduler')) {
+             return this.get('controller.scheduler.inProgress').indexOf(this.get('key')) !== -1;
+             }
+             else {
+             return false;
+             }
+             }.property('controller.scheduler.inProgress.length')
+             */
+        })
+
     })
 
 });
