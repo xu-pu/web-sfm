ce0564da4b1ee49b8afa90b0619684d783b030b2
ptx-pluto <sheep9275@gmail.com>
2015-02-21 00:39:12 +0800

    comet pair prepared

--- tests/external-test.js
+++ tests/external-test.js
@@ -1,27 +1,21 @@
 'use strict';
 
 var _ = require('underscore'),
-    Promise = require('promise'),
-    blur = require('ndarray-gaussian-filter');
+    Promise = require('promise');
 
 var imgUtils = require('../src/utils/image-conversion.js'),
     samples = require('../src/utils/samples.js'),
     visualUtils = require('../src/utils/testing.js'),
     testUtils = require('../src/utils/test-utils.js'),
-    isNotEdge = require('../src/websift/edge-filter.js'),
-    OctaveSpace = require('../src/websift/octave-space'),
-    detector = require('../src/websift/detector.js'),
-    orientation = require('../src/websift/orientation.js'),
-    descriptor = require('../src/websift/descriptor.js'),
-    sift = require('../src/websift/websift.js'),
     externalUtils = require('../src/utils/external-utils.js');
 
-
-var cometRoot = '/home/sheep/Downloads/comet/',
+var cometRoot = '/home/sheep/Downloads/comet-demo/',
     smallComet = 'Colour_image_of_comet.jpg',
     bigComet = 'Comet_on_5_September_2014.jpg',
     smallpic = '/home/sheep/Code/Project/web-sfm/tests/images/ibzi0xiqN0on8v.jpg',
-    samplepic = '/home/sheep/Code/Project/web-sfm/tests/images/ibzi0xiqN0on8v.jpg';
+    samplepic = '/home/sheep/Code/Project/web-sfm/tests/images/ibzi0xiqN0on8v.jpg',
+    cometPair1 = 'Comet_on_7_August_a.jpg',
+    cometPair2 = 'Comet_on_7_August_b.jpg';
 
 
 function testLoweSIFT(path){
@@ -31,4 +25,28 @@ function testLoweSIFT(path){
         });
 }
 
-testLoweSIFT(cometRoot+bigComet);
+//testLoweSIFT(cometRoot+cometPair1);
+
+function preparePair(){
+
+    var pathA = cometRoot + cometPair1,
+        pathB = cometRoot + cometPair2;
+
+    externalUtils.loweSIFT(pathA)
+        .then(function(featuresA){
+            return testUtils.promiseSaveJson('/home/sheep/Code/comet-pair-A.json', featuresA);
+        })
+        .then(function(){
+            return externalUtils.loweSIFT(pathB);
+        })
+        .then(function(featuresB){
+            return testUtils.promiseSaveJson('/home/sheep/Code/comet-pair-B.json', featuresB);
+        })
+        .then(function(){
+            console.log('features generated');
+        });
+
+}
+//preparePair();
+//testUtils.promiseVisualPoints('/home/sheep/Code/comet-pair-A.png', cometRoot + cometPair1, require('/home/sheep/Code/comet-pair-A.json'));
+//testUtils.promiseVisualPoints('/home/sheep/Code/comet-pair-B.png', cometRoot + cometPair2, require('/home/sheep/Code/comet-pair-B.json'));
