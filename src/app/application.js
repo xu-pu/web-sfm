4b184263cb730c39ed19d2a20e45523c216984f7
ptx-pluto <sheep9275@gmail.com>
2015-03-04 16:36:04 +0800

    view setups moved to application.js

--- src/app/application.js
+++ src/app/application.js
@@ -1,8 +1,7 @@
 'use strict';
 
 var setupControllers = require('./controllers.js'),
-    setupRoutes = require('./router.js'),
-    setupViews = require('./views.js');
+    setupRoutes = require('./router.js');
 
 window.Promise = Promise || Ember.RSVP.Promise;
 jQuery.event.props.push( "dataTransfer" );
@@ -20,6 +19,21 @@ App.MultiViewStereoComponent = require('./components/MultiViewStereo.js');
 App.SparseReconstructionComponent = require('./components/SparseReconstruction.js');
 
 
+//============================================
+// Views
+//============================================
+
+App.WelcomeDemoView = require('./RouteViews/WelcomeDemoView.js');
+App.WorkspaceMatcherView = require('./RouteViews/WorkspaceMatcherView.js');
+App.ProjectThumbnailView = require('./views/ProjectThumbnailView.js');
+App.DemoThumbnailView = require('./views/DemoThumbnailView.js');
+App.ImageLoaderView = require('./views/ImageLoaderView.js');
+App.CpuSettingView = require('./views/CpuSettingView.js');
+App.SiftView = require('./views/SiftView.js');
+App.StateBarView = require('./views/StateBarView.js');
+App.TwoViewMatchingView = require('./views/TwoViewMatchingView.js');
+App.ProjectCreatorView = require('./views/ProjectCreatorView.js');
+
+
 setupRoutes(App);
 setupControllers(App);
-setupViews(App);
