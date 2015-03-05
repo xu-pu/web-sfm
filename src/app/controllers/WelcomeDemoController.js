ea543d8fbef66b88a30201ca5933073d75d8d2e4
ptx-pluto <sheep9275@gmail.com>
2015-03-05 17:52:07 +0800

    selective download removed

--- src/app/controllers/WelcomeDemoController.js
+++ src/app/controllers/WelcomeDemoController.js
@@ -5,7 +5,7 @@ var settings = require('../settings.js'),
 
 module.exports = Ember.ObjectController.extend({
 
-    needs: ['demos'],
+    needs: ['demos', 'downloader'],
 
     demoController: null,
 
@@ -21,57 +21,8 @@ module.exports = Ember.ObjectController.extend({
 
     actions: {
 
-        progress: function(){
-            if (!this.get('isInprogress')) {
-                if (this.get('isReady')) {
-                    this.get('demoController').send('enter');
-                }
-                else {
-                    this.get('demoController').send('download');
-                }
-            }
-        },
-
-        toggleSelection: function(entry){
-
-            var entries = this.get('entries'),
-                selected = this.get('selectedEntries'),
-                loaded = this.get('loadedEntries');
-
-            if (this.get('isInprogress') || !entries.contains(entry)) {
-                return;
-            }
-
-            if (!selected.contains(entry)) {
-                selected.addObject(entry);
-            }
-            else {
-
-                var needDelete = false;
-
-                switch (entry) {
-                    case ENTRIES.IMAGE:
-                        needDelete = this.get('loadedImages.length') > 0;
-                        break;
-                    case ENTRIES.FEATURE:
-                        needDelete = this.get('loadedFeatures.length') > 0;
-                        break;
-                    default:
-                        needDelete = loaded.contains(entry);
-                        break;
-                }
-
-                if (needDelete) {
-                    if (window.confirm('Delete loaded '+ entries + '?')) {
-                        selected.removeObject(entry);
-                    }
-                }
-                else {
-                    selected.removeObject(entry);
-                }
-
-            }
-
+        enter: function(){
+            this.get('demoController').send('enter');
         }
 
     }
