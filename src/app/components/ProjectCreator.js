d09eab4c5ae1466ca3c6a4d2d82d63bf90ddefa0
ptx-pluto <sheep9275@gmail.com>
2015-03-06 00:56:21 +0800

    enhanced project creator

--- src/app/components/ProjectCreator.js
+++ src/app/components/ProjectCreator.js
@@ -12,22 +12,43 @@ module.exports = Ember.Component.extend({
 
     classNameBindings: ['isInvalid'],
 
+    defaultName: 'my-project',
+
     newProjectName: '',
 
     isInvalid: function(){
         var name = this.get('newProjectName'),
             ctx = this.get('ctx'),
-            isValid = name !== '' && ctx.nameAvaliable(name);
+            isValid = ctx.nameAvaliable(name) || name === '';
         return !isValid;
     }.property('newProjectName'),
 
     actions: {
 
         createProject: function(){
+
+            var defaultName = this.get('defaultName'),
+                name = this.get('newProjectName'),
+                ctx = this.get('ctx');
+
             if (!this.get('isInvalid')) {
-                var project = Project.create({ name: this.get('newProjectName') });
-                this.get('ctx').send('createProject', project);
+                if (name === '') {
+                    name = getNextDefaultName();
+                }
+                var project = Project.create({ name: name });
+                ctx.send('createProject', project);
             }
+
+            function getNextDefaultName(i){
+                if (i) {
+                    var name = defaultName+'-'+i;
+                    return ctx.nameAvaliable(name) ? name : getNextDefaultName(i+1);
+                }
+                else {
+                    return ctx.nameAvaliable(defaultName) ? defaultName : getNextDefaultName(2);
+                }
+            }
+
         }
 
     }
