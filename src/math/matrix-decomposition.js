34003c200c20618ab7772f8ea009701e3bf498df
ptx-pluto <sheep9275@gmail.com>
2015-03-24 16:21:01 +0800

    QR and RQ decompostion added

---
+++ src/math/matrix-decomposition.js
@@ -0,0 +1,82 @@
+var _ = require('underscore'),
+    la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector;
+
+//=======================================================
+
+/**
+ *
+ * @param {Matrix} m
+ * @returns {{ Q: Matrix, R: Matrix }}
+ */
+exports.qrDecomposition3d = function(m){
+
+    var a1 = m.col(1), a2 = m.col(2), a3 = m.col(3);
+
+    var u1 = a1,
+        e1 = u1.x(1/u1.modulus());
+
+    var u2 = a2.subtract(param(a2, e1)),
+        e2 = u2.x(1/u2.modulus());
+
+    var u3 = a3.subtract(param(a3, e1)).subtract(param(a3, e2)),
+        e3 = u3.x(1/u3.modulus());
+
+    var Q = Matrix.create([e1.elements, e2.elements, e3.elements]).transpose(),
+        R = Matrix.create([
+            [ dot(e1,a1), dot(e1,a2), dot(e1,a3)],
+            [ 0         , dot(e2,a2), dot(e2,a3)],
+            [ 0         , 0         , dot(e3,a3)]
+        ]);
+
+
+    function dot(x,y){
+        return x.dot(y);
+    }
+
+    function param(a, e){
+        return e.x(dot(e,a)/dot(e,e));
+    }
+
+    return { Q: Q, R: R };
+
+};
+
+/**
+ *
+ * @param {Matrix} m
+ * @returns {{ Q: Matrix, R: Matrix }}
+ */
+exports.rqDecomposition3d = function(m){
+
+    var a1 = m.row(3), a2 = m.row(2), a3 = m.row(1);
+
+    var u1 = a1,
+        e1 = u1.x(1/u1.modulus());
+
+    var u2 = a2.subtract(param(a2, e1)),
+        e2 = u2.x(1/u2.modulus());
+
+    var u3 = a3.subtract(param(a3, e1)).subtract(param(a3, e2)),
+        e3 = u3.x(1/u3.modulus());
+
+    var Q = Matrix.create([e3.elements, e2.elements, e1.elements]),
+        R = Matrix.create([
+            [ dot(e3,a3), dot(e2,a3), dot(e1,a3)],
+            [ 0         , dot(e2,a2), dot(e1,a2)],
+            [ 0         , 0         , dot(e1,a1)]
+        ]);
+
+
+    function dot(x,y){
+        return x.dot(y);
+    }
+
+    function param(a, e){
+        return e.x(dot(e,a)/dot(e,e));
+    }
+
+    return { Q: Q, R: R };
+
+};
