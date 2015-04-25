bda08a95573961be6eb50950a3b7352aad867665
ptx-pluto <sheep9275@gmail.com>
2015-04-25 23:53:59 +0800

    sba matrix generator added

---
+++ src/utils/random-test-utils.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var _ = require('underscore'),
+    la = require('sylvester'),
+    Matrix = la.Matrix,
+    Vector = la.Vector;
+
+var laUtils = require('../math/la-utils.js'),
+    sparse = require('../math/sparse-matrix.js'),
+    SparseMatrix = sparse.SparseMatrix;
+
+
+var CAM_PARAMS = 11;
+var POINT_PARAMS = 3;
+
+
+/**
+ * [ A , C ]
+ * [ C', B ]
+ * @param {int} cams
+ * @param {int} points
+ * @returns SparseMatrix
+ */
+exports.genSBAMatrix = function(cams, points){
+
+    var sizeA = CAM_PARAMS*cams;
+    var sizeB = POINT_PARAMS*points;
+    var size = sizeA + sizeB;
+
+    var sparseA = SparseMatrix.fromBlockDiag(_.range(cams).map(function(){
+        return Matrix.Random(CAM_PARAMS, CAM_PARAMS).elements;
+    }));
+
+    var sparseB = SparseMatrix.fromBlockDiag(_.range(points).map(function(){
+        return Matrix.Random(POINT_PARAMS, POINT_PARAMS).elements;
+    }));
+
+    var sparseC = SparseMatrix.Random(sizeA, sizeB, 0.3);
+
+    var denseC = sparseC.toDense();
+    var denseA = sparseA.toDense();
+    var denseB = sparseB.toDense();
+
+    var r, c, buffer = Matrix.Zero(size, size).elements;
+    for (r=0; r<size; r++) {
+        for (c=0; c<size; c++) {
+            if (r<sizeA && c<sizeA) {
+                // A block
+                buffer[r][c] = denseA[r][c];
+            }
+            else if (r<sizeA && c>=sizeA) {
+                // C block
+                buffer[r][c] = denseC[r][c-sizeA];
+            }
+            else if (r>=sizeA && c<sizeA) {
+                // C' block
+                buffer[r][c] = denseC[c][r-sizeA];
+            }
+            else {
+                // B block
+                buffer[r][c] = denseB[r-sizeA][c-sizeA];
+            }
+        }
+    }
+
+    return SparseMatrix.fromDense(buffer);
+
+};
