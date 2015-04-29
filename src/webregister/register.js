7e577a40ce04540b5aa4ee8949da4a8af006c61c
ptx-pluto <sheep9275@gmail.com>
2015-04-29 21:28:09 +0800

    register context created

--- src/webregister/register.js
+++ src/webregister/register.js
@@ -1,7 +1,7 @@
 var _ = require('underscore');
 
-var lma = require('../math/nonlinear.js').lma,
-    estP = require('./estimate-projection');
+var estP = require('./estimate-projection'),
+    sba = require('./sparse-bundle-adjustment.js');
 
 //============================================
 
@@ -33,7 +33,6 @@ exports.getCommonTracks = function(camInds, tracks){
 
 exports.RegisterContext = RegisterContext;
 
-
 /**
  *
  * @param {Track[]} tracks
@@ -43,15 +42,83 @@ function RegisterContext(tracks){
     this.tracks = tracks;
     this.xDict = {};
     this.camDict = {};
+    this.excluded = [];
 }
 
+
+/**
+ *
+ * @param {int} i
+ * @param {CameraParams} c
+ */
+RegisterContext.prototype.addCamera = function(i, c){
+    this.camDict[i] = c;
+};
+
+
+/**
+ *
+ * @param {int} i
+ * @param {Vector} X
+ */
+RegisterContext.prototype.addPoint = function(i, X){
+    this.xDict[i] = X;
+};
+
+
+/**
+ *
+ * @param {{ xi: int, X: Vector }[]} arr
+ */
+RegisterContext.prototype.addPointsArr = function(arr){
+    var xDict = this.xDict;
+    arr.forEach(function(entry){
+        xDict[entry.xi] = entry.X;
+    });
+};
+
+
+/**
+ *
+ * @param dict
+ */
+RegisterContext.prototype.addPointDict = function(dict){
+    _.extend(this.xDict, dict);
+};
+
+
+/**
+ *
+ * @param {int} i
+ */
+RegisterContext.prototype.excludeTrack = function(i){
+    delete this.xDict[i];
+    var excluded = this.excluded;
+    if (excluded.indexOf(i) === -1) {
+        excluded.push(i);
+    }
+};
+
+
+/**
+ * Bundle Adjustment (global by default)
+ * @param {int[]} [cams]
+ * @param {int[]} [points]
+ */
+RegisterContext.prototype.adjust = function(cams, points){
+    var tracks = this.tracks,
+        camDict = this.camDict,
+        xDict = this.xDict;
+    cams = cams || _.keys(camDict).map(key2int);
+    points = points || _.keys(xDict).map(key2int);
+    sba.sba(camDict, xDict, tracks, cams, points);
+};
 
 /**
  * @returns int[]
  */
 RegisterContext.prototype.getRecoveredTrackInds = function(){
-    return _.keys(this.xDict)
-        .map(function(keyStr){
-            return parseInt(keyStr, 10);
-        });
+    return _.keys(this.xDict).map(key2int);
 };
+
+function key2int(key){ return parseInt(key, 10); }
