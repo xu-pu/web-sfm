1e6a95ee44caec5b729e9eddd9a47ff4c42a5804
ptx-pluto <sheep9275@gmail.com>
2015-05-01 15:39:13 +0800

    robust sparse bundle adjustment added

--- src/webregister/register.js
+++ src/webregister/register.js
@@ -2,6 +2,7 @@ var _ = require('underscore');
 
 var camUtils = require('../math/projections.js'),
     laUtils = require('../math/la-utils.js'),
+    geoUtils = require('../math/geometry-utils.js'),
     shortcuts = require('../utils/shortcuts.js'),
     cord = require('../utils/cord.js'),
     triangulation = require('./triangulation.js'),
@@ -9,6 +10,7 @@ var camUtils = require('../math/projections.js'),
     sba = require('./sparse-bundle-adjustment.js');
 
 var TRIANGULATION_CUTOFF = 0.2;
+var TRACK_OUTLIER_CUTOFF = 0.8;
 
 //============================================
 
@@ -103,6 +105,98 @@ RegisterContext.prototype.adjust = function(cams, points){
 
 
 /**
+ * Robust Bundle Adjustment with oulier filter
+ * @param {int[]} [cams]
+ * @param {int[]} [points]
+ * @returns {{ inliers: int[], outliers: int[] }}
+ */
+RegisterContext.prototype.robustAdjust = function(cams, points){
+
+    cams = cams || _.keys(this.camDict).map(key2int);
+    points = points || _.keys(this.xDict).map(key2int);
+
+    var tracks = this.tracks;
+    var ctx = this;
+    var inliers = points.slice();
+    var outliers = [];
+    var filtered;
+
+    do {
+
+        ctx.adjust(cams, inliers);
+        filtered = findOutliers();
+        inliers = _.difference(inliers, filtered);
+        outliers = outliers.concat(filtered);
+        filtered.forEach(function(xi){ ctx.excludeTrack(xi); });
+        console.log(filtered);
+        console.log(filtered.length);
+
+    } while (filtered.length>0);
+
+    return { inliers: inliers, outliers: outliers };
+
+    function findOutliers(){
+
+        var cDict = ctx.camDict;
+        var xDict = ctx.xDict;
+        var pDict = _.mapObject(cDict, function(params){
+            return camUtils.params2P(params);
+        });
+        var vDict = inliers.reduce(function(memo, xi){
+            tracks[xi].forEach(function(view){
+                var ci = view.cam,
+                    rc = view.point;
+                if (cams.indexOf(ci) !== -1) {
+                    memo[ci].push({ xi: xi, rc: rc });
+                }
+            });
+            return memo;
+        }, _.mapObject(cDict, function(){ return []; }));
+
+
+        return _.union.apply(null, cams.map(function(ci){
+            return findCamOutliers(ci);
+        }));
+
+        function findCamOutliers(ci){
+
+            var P = pDict[ci];
+
+            /** @type {{ xi: int, error: number }[]} */
+            var errorl = vDict[ci].map(function(entry){
+                var xi = entry.xi,
+                    rc = entry.rc,
+                    X = xDict[xi],
+                    x = P.x(X),
+                    rc2 = cord.img2RC(x);
+                return { xi: xi, error: geoUtils.getDistanceRC(rc, rc2) };
+            });
+
+            errorl.sort(function(l,r){ return l.error > r.error; });
+
+            var d80 = errorl[Math.floor(errorl.length*TRACK_OUTLIER_CUTOFF)].error;
+
+            function clamp(x, a, b){ return Math.min(Math.max(x,a),b); }
+
+            var threshold = clamp(2.4*d80, 4, 16);
+
+            console.log('d80: ' + d80);
+            console.log('thresh: ' + threshold);
+
+            return errorl.reduce(function(memo, entry){
+                if (entry.error >= threshold) {
+                    memo.push(entry.xi);
+                }
+                return memo;
+            }, []);
+        }
+
+    }
+
+};
+
+
+/**
  *
  * @param {int[]} inds
  * @returns {int[]}
@@ -151,7 +245,7 @@ RegisterContext.prototype.attemptTriangulation = function(inds){
             }
         });
 
-        console.log(maximum);
+        //console.log(maximum);
 
         if (maximum<TRIANGULATION_CUTOFF) { return; }
 
@@ -162,8 +256,10 @@ RegisterContext.prototype.attemptTriangulation = function(inds){
 
         xDict[xi] = triangulation(P1, P2, x1, x2);
         triangulated.push(xi);
-        console.log('triangulated one');
+        //console.log('triangulated one');
     });
+
+    console.log('triangulated ' + triangulated.length);
 
     return triangulated;
 
