98bae01c5229e3141cfd9cb12731f0d626487102
ptx-pluto <sheep9275@gmail.com>
2015-05-01 11:12:18 +0800

    attenmpt triangulation added

--- src/webregister/register.js
+++ src/webregister/register.js
@@ -1,32 +1,19 @@
 var _ = require('underscore');
 
-var estP = require('./estimate-projection'),
+var camUtils = require('../math/projections.js'),
+    laUtils = require('../math/la-utils.js'),
+    shortcuts = require('../utils/shortcuts.js'),
+    cord = require('../utils/cord.js'),
+    triangulation = require('./triangulation.js'),
+    estP = require('./estimate-projection'),
     sba = require('./sparse-bundle-adjustment.js');
+
+var TRIANGULATION_CUTOFF = 0.2;
 
 //============================================
 
 exports.register = function(tracks, Fs){
 
-};
-
-/**
- *
- * @param {int[]} camInds
- * @param {Track[]} tracks
- * @returns int[] - track ids
- */
-exports.getCommonTracks = function(camInds, tracks){
-    return tracks.reduce(function(memo, track, ind){
-        var isVisAll = camInds.every(function(camInd){
-            return track.some(function(view){
-                return view.cam === camInd;
-            });
-        });
-        if (isVisAll) {
-            memo.push(ind);
-        }
-        return memo;
-    }, []);
 };
 
 //============================================
@@ -114,6 +101,90 @@ RegisterContext.prototype.adjust = function(cams, points){
     sba.sba(camDict, xDict, tracks, cams, points);
 };
 
+
+/**
+ *
+ * @param {int[]} inds
+ * @returns {int[]}
+ */
+RegisterContext.prototype.attemptTriangulation = function(inds){
+
+    var tracks = this.tracks;
+    var cDict = this.camDict; // CameraParams
+    var xDict = this.xDict; // 4D Homogenous
+    var mDict = _.mapObject(cDict, function(params){ return camUtils.params2model(params); }); // CameraModel
+    var rDict = _.mapObject(mDict, function(model){ return camUtils.getPerspective(model.R, model.t).inverse(); }); // Reverse Perspective Transform Matrix
+    var tDict = _.mapObject(mDict, function(model){ return camUtils.model2T(model); }); // T
+    var pDict = _.mapObject(mDict, function(model){ return camUtils.model2P(model); }); // Projection Matrix
+
+    var triangulated = [];
+
+    /**
+     * @typedef {{ rc: RowCol, ci: int, v: Vector }} Ray
+     */
+
+    inds.forEach(function(xi){
+
+        var track = tracks[xi];
+
+        /** @type {Ray[]} */
+        var rays = track.reduce(function(memo, view){
+            var ci = view.cam;
+            if (cDict[ci]) {
+                var X = rc2X(view.point, ci);
+                var T = tDict[ci];
+                memo.push({
+                    rc: view.point,
+                    ci: ci,
+                    v: T.subtract(X)
+                });
+            }
+            return memo;
+        }, []);
+
+        var maximum=-Infinity, pair=[];
+        shortcuts.iterPairs(rays, function(ray1, ray2){
+            var angle = ray1.v.angleFrom(ray2.v);
+            if (angle>maximum) {
+                maximum = angle;
+                pair = [ray1, ray2];
+            }
+        });
+
+        console.log(maximum);
+
+        if (maximum<TRIANGULATION_CUTOFF) { return; }
+
+        var P1 = pDict[pair[0].ci];
+        var P2 = pDict[pair[1].ci];
+        var x1 = cord.rc2x(pair[0].rc);
+        var x2 = cord.rc2x(pair[1].rc);
+
+        xDict[xi] = triangulation(P1, P2, x1, x2);
+        triangulated.push(xi);
+        console.log('triangulated one');
+    });
+
+    return triangulated;
+
+    /**
+     *
+     * @param {RowCol} rc
+     * @param {int} ci
+     * @returns {Vector} -- 3D Hetero
+     */
+    function rc2X(rc, ci){
+        /** @type {CameraParams} */
+        var params = cDict[ci];
+        var perspective = laUtils.toVector([ rc.col-params.px, rc.row-params.py, params.f, 1 ]);
+        var X = rDict[ci].x(perspective);
+        return cord.toInhomo3D(X);
+    }
+
+
+};
+
+
 /**
  * @returns int[]
  */
@@ -122,3 +193,26 @@ RegisterContext.prototype.getRecoveredTrackInds = function(){
 };
 
 function key2int(key){ return parseInt(key, 10); }
+
+
+//===============================================================
+
+/**
+ *
+ * @param {int[]} camInds
+ * @param {Track[]} tracks
+ * @returns int[] - track ids
+ */
+exports.getCommonTracks = function(camInds, tracks){
+    return tracks.reduce(function(memo, track, ind){
+        var isVisAll = camInds.every(function(camInd){
+            return track.some(function(view){
+                return view.cam === camInd;
+            });
+        });
+        if (isVisAll) {
+            memo.push(ind);
+        }
+        return memo;
+    }, []);
+};
