588adc2b366a5d483ccc4b20bc654c42948f2b9e
ptx-pluto <sheep9275@gmail.com>
2015-05-03 00:11:53 +0800

    merged

--- src/visualization/vis3d.js
+++ src/visualization/vis3d.js
@@ -7,6 +7,23 @@ var THREE = require('three'),
 
 var laUtils = require('../math/la-utils.js');
 
+/**
+ *
+ * @returns {THREE.Line}
+ */
+exports.getCoordinateFrame = function(){
+    var axisGeo = new THREE.Geometry();
+    axisGeo.vertices = [
+        new THREE.Vector3(0,0,0), new THREE.Vector3(500,0,0),
+        new THREE.Vector3(0,0,0), new THREE.Vector3(0,500,0),
+        new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,500)
+    ];
+    var axisMaterial = new THREE.LineBasicMaterial({
+        color: 0x464646
+    });
+    return new THREE.Line(axisGeo, axisMaterial, THREE.LinePieces);
+};
+
 
 /**
  * @param {CalibratedCamera} camera
