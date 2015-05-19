2fa83a89607f2fc3168cb77c345245431df46e36
ptx-pluto <sheep9275@gmail.com>
2015-05-19 22:13:18 +0800

    more images added to comet demo

--- demo/Rosetta-Spacecraft/description.js
+++ demo/Rosetta-Spacecraft/description.js
@@ -17,20 +17,24 @@ module.exports = {
     { "id": 9, "name": "Comet_on_19_September_2014_NavCam", "extension": ".jpg" },
     { "id": 10, "name": "Comet_on_22_August_2014_-_NavCam", "extension": ".jpg" },
     { "id": 11, "name": "Comet_on_24_September_NavCam", "extension": ".jpg" },
-
     { "id": 12, "name": "Comet_on_4_November_NavCam", "extension": ".jpg"},
     { "id": 13, "name": "Comet_on_12_January_2015_NavCam", "extension": ".jpg"},
     { "id": 14, "name": "ESA_Rosetta_Navcam_20150122_mosaic-1024x906", "extension": ".jpg"},
     { "id": 15, "name": "ESA_Rosetta_NavCam_20150131_Mosaic-1024x958", "extension": ".jpg"},
-    { "id": 16, "name": "ESA_Rosetta_NavCam_20150203_Mosaic-938x1024", "extension": ".jpg"}
+    { "id": 16, "name": "ESA_Rosetta_NavCam_20150203_Mosaic-938x1024", "extension": ".jpg"},
 
-/*
-    { "id": , "name": "", "extension": ""},
-    { "id": , "name": "", "extension": ""},
-    { "id": , "name": "", "extension": ""},
-    { "id": , "name": "", "extension": ""},
-    { "id": , "name": "", "extension": ""},
-    { "id": , "name": "", "extension": ""},
+    { "id": 17, "name": "ESA_Rosetta_NAVCAM_20150118_mosaic-950x1024", "extension": ".jpg"},
+    { "id": 18, "name": "ESA_Rosetta_NAVCAM_20150121_mosaic-1024x969", "extension": ".jpg"},
+    { "id": 19, "name": "False_colour_comet_Hapi_region", "extension": ".jpg"},
+    { "id": 20, "name": "Comet_on_21_August_2014_-_NavCam", "extension": ".jpg"},
+    { "id": 21, "name": "Comet_on_17_August_2014_-_NavCam", "extension": ".jpg"},
+    { "id": 22, "name": "Comet_on_10_January_2015_NavCam", "extension": ".jpg"},
+    { "id": 23, "name": "Comet_on_1_January_2015_NavCam", "extension": ".jpg"},
+    { "id": 24, "name": "Comet_on_2_December_NavCam", "extension": ".jpg"},
+    { "id": 25, "name": "Agilkia_landing_site_6_November_2014", "extension": ".jpg"},
+    { "id": 26, "name": "Comet_on_1_December_2014_NavCam", "extension": ".jpg"}
+
+      /*
     { "id": , "name": "", "extension": ""},
 */
 
