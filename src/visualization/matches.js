d5582288ee2dff33f1592e7d79896ff5b7e57ddc
ptx-pluto <sheep9275@gmail.com>
2015-05-21 17:22:10 +0800

    two-view visual fixed

--- src/visualization/matches.js
+++ src/visualization/matches.js
@@ -23,26 +23,32 @@ var cord = require('../utils/cord.js');
  */
 exports.drawImagePair = function(img1, img2, canvas, fixedWidth){
     var PADDING = 10;
-    var ratioX = img1.height/img1.width + img2.height/img2.width;
-    var ratioY = img1.width/img1.height + img2.width/img2.height;
-    var alignX = (ratioX>1 ? ratioX : 1/ratioX) <= (ratioY>1 ? ratioY : 1/ratioY);
-    var ratio1 = alignX ? fixedWidth/img1.width : (fixedWidth/ratioY)/img1.height,
-        ratio2 = alignX ? fixedWidth/img2.width : (fixedWidth/ratioY)/img2.height;
+    var heightV = img1.height/img1.width + img2.height/img2.width;
+    var widthH = img1.width/img1.height + img2.width/img2.height;
+    var alignV = (heightV>1 ? heightV : 1/heightV) <= (widthH>1 ? widthH : 1/widthH);
+    var ratio1 = alignV ? fixedWidth/img1.width : ((fixedWidth-PADDING)/widthH)/img1.height,
+        ratio2 = alignV ? fixedWidth/img2.width : ((fixedWidth-PADDING)/widthH)/img2.height;
 
     canvas.width = fixedWidth;
-    canvas.height = alignX ? ratioX*fixedWidth : fixedWidth/ratioY;
+    canvas.height = alignV ? heightV*fixedWidth : fixedWidth/widthH;
+
+    var width1 = img1.width*ratio1,
+        height1 = img1.height*ratio1,
+        width2 = img2.width*ratio2,
+        height2 = img2.height*ratio2;
+
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage(img1, 0, 0, img1.width*ratio1, img1.height*ratio1);
-    if (alignX) {
-        ctx.drawImage(img2, 0, img1.height*ratio1+PADDING, img2.width*ratio1, img2.height*ratio1);
+    ctx.drawImage(img1, 0, 0, width1, height1);
+    if (alignV) {
+        ctx.drawImage(img2, 0, height1+PADDING, width2, height2);
     }
     else {
-        ctx.drawImage(img2, img1.width*ratio1+PADDING, 0, img2.width*ratio1, img2.height*ratio1);
+        ctx.drawImage(img2, width1+PADDING, 0, width2, height2);
     }
 
     var offsetX, offsetY;
-    if (alignX) {
+    if (alignV) {
         offsetX = 0;
         offsetY = img1.height * ratio1 + PADDING;
     }
@@ -52,7 +58,7 @@ exports.drawImagePair = function(img1, img2, canvas, fixedWidth){
     }
 
     return {
-        alignX: alignX,
+        alignX: alignV,
         ratio1: ratio1,
         ratio2: ratio2,
         padding: PADDING,
